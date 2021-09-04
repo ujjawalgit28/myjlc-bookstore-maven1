f9cf8c45e1ab0250a2b98d007488c70ff4476bc8
Srinivas Dande <srinivas@coursecube.com>
2021-09-04 08:42:48 +0530

    first commit of jlc bookstore

---
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -0,0 +1,13 @@
+package com.jlcindia.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApplication {
+public static void main(String[] args) {
+System.out.println("Starting Boot Application");
+System.out.println("Starting Boot Application");
+SpringApplication.run(MyBootApplication.class, args);
+}
+}
