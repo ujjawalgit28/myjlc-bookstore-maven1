77dcf52c751845965919384a8e209745e0ec3ce2
DandesClasses <87064600+DandesClasses@users.noreply.github.com>
2021-09-18 10:14:01 +0530

    Update -1 Srinivas

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyBootApplication {
 public static void main(String[] args) {
 System.out.println("Starting Boot Application");
-System.out.println("Starting Boot Application");
+System.out.println("Update -1 Srinivas");
+  
 SpringApplication.run(MyBootApplication.class, args);
 }
 }
