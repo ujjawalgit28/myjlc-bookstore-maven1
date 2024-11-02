6634ea4f59e9dc788849c3b9e2404fb62d2156d5
ujjawalgit28 <puauas@gmail.com>
2024-11-02 22:09:57 +0530

    Update MyBootApplication.java by 6th update

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -14,6 +14,7 @@ System.out.println("Update -1 Srinivas");
   System.out.println("Update -4 by Self");
   System.out.println("Update -5 by Self");
   System.out.println("Update -5 by Self");
+  System.out.println("Update -6 by Self");
 
 SpringApplication.run(MyBootApplication.class, args);
 }
