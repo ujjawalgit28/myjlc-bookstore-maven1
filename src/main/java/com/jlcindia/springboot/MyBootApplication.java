133f944424ded468b2d26af4ae7e01ae333f3162
ujjawalgit28 <puauas@gmail.com>
2024-11-02 16:15:55 +0530

    Update MyBootApplication.java by self25

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -10,6 +10,7 @@ System.out.println("Starting Boot Application");
 System.out.println("Update -1 Srinivas");
   System.out.println("Update -2 by Srinivas");
   System.out.println("Update -2 by Self");
+  System.out.println("Update -3 by Self");
 
 SpringApplication.run(MyBootApplication.class, args);
 }
